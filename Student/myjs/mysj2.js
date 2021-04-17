025558cc190f59e28ce69eabd181994ddfc23306
Reharsh <reharshdeshpande@gmail.com>
2021-04-17 12:23:06 +0530

    Dump CSS and JS

---
+++ Student/myjs/mysj2.js
@@ -0,0 +1,10 @@
+﻿function ShowPopup(myMsg, myTitle) {
+    $("#MyPopup .modal-title").html(myTitle);
+    $("#MyPopup .modal-body").html(myMsg);
+    $("#MyPopup").modal("show");
+}
+function ShowPopup1(myMsg, myTitle) {
+    $("#MyPopup1 .modal-title").html(myTitle);
+    $("#MyPopup1 .modal-body").html(myMsg);
+    $("#MyPopup1").modal("show");
+}
